e4ff89cf9979829ba03ffd5e134cb145ac2b2308
Jeffery Eckert <bromodo@gmail.com>
2019-09-30 19:38:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,4 +8,8 @@
 # return reversed_string.join('')
 # end
 
-def reverse_each_word(array) new_array = array.split(" ") new_array.collect {|x| x.reverse!} new_array.join(" ") end
+def reverse_each_word(array) 
+  new_array = array.split(" ") 
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ") 
+  end
