0d462a6d15a45e5b012cf005ee30f98c479d33c3
Jeffery Eckert <bromodo@gmail.com>
2019-09-30 19:39:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,7 +8,11 @@
 # return reversed_string.join('')
 # end
 
-
+def reverse_each_word(array)
+  new_array = array.split(" ") 
+  reversed_array = new_array.each {|x| x.reverse!} 
+  return reversed_array.join(" ") 
+  end
 
 
 
