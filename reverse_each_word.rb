4820f8a543d3b8c6a6b6cf9a573ff8864dc728a6
Jeffery Eckert <bromodo@gmail.com>
2019-09-30 19:25:17 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  string = string.split('')
+  reversed_string = []
+  
+  string.each do |char|
+    reversed_string.unshift(char)
+end
+return reversed_string.join('')
+end
